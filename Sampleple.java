9958ebc6b6f34557b6e05b6a36e4c1098dc5c0e8
yoshimasa-m <magamagatantan@gmail.com>
2015-10-08 19:48:49 +0900

    もっかいてすと

---
+++ Sampleple.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Lesson15{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		String first = sc.next();
+		String second = sc.next();
+		
+		if(first.equals(second)){
+			System.out.println("“¯‚¶’l‚Å‚·");
+		}else{
+			System.out.println("ˆá‚¤’l‚Å‚·");
+		}
+	}
+}
